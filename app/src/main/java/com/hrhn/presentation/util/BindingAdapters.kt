adda187dc5e51f3299c87d1843b8cbafd79cc47c
oreocube <oreocube13@gmail.com>
2022-12-23 11:13:48 +0900

    ui: 체크 이모지 아이템 구현 #6

--- app/src/main/java/com/hrhn/presentation/util/BindingAdapters.kt
+++ app/src/main/java/com/hrhn/presentation/util/BindingAdapters.kt
@@ -1,6 +1,8 @@
 package com.hrhn.presentation.util
 
+import android.view.View
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -13,4 +15,9 @@ fun TextView.setDateString(date: LocalDateTime) {
 @BindingAdapter("android:dateWithYear")
 fun TextView.setDateWithYearString(date: LocalDateTime) {
     this.text = date.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
+}
+
+@BindingAdapter("android:visibleIf")
+fun View.setVisible(boolean: Boolean) {
+    isVisible = boolean
 }
