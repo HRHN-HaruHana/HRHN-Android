3661257a01d1d04815cbf860c1ea00e5a9cdc430
oreocube <oreocube13@gmail.com>
2022-12-26 21:05:54 +0900

    feat: 설정 화면 SharedPreference 연결 #17

--- app/src/main/java/com/hrhn/presentation/ui/screen/setting/SettingFragment.kt
+++ app/src/main/java/com/hrhn/presentation/ui/screen/setting/SettingFragment.kt
@@ -1,11 +1,27 @@
 package com.hrhn.presentation.ui.screen.setting
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import com.hrhn.R
 
-class SettingFragment : PreferenceFragmentCompat() {
+class SettingFragment : PreferenceFragmentCompat(),
+    SharedPreferences.OnSharedPreferenceChangeListener {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        // TODO
     }
 }
