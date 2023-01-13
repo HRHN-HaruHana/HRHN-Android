92a2b0f505615a7526a49985e3c786fc1e5f4876
oreocube <oreocube13@gmail.com>
2023-01-13 17:09:05 +0900

    fix: 네비게이션 버그 수정
    - 지난 챌린지 화면에서 이모지 수정 시 프래그먼트 전환 버그
    - fragment result api 사용하여 각 activity에서 업데이트 성공 이벤트를 다르게 처리

--- app/src/main/java/com/hrhn/presentation/ui/screen/review/ReviewActivity.kt
+++ app/src/main/java/com/hrhn/presentation/ui/screen/review/ReviewActivity.kt
@@ -4,15 +4,13 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.hrhn.R
 import com.hrhn.databinding.ActivityReviewBinding
 import com.hrhn.domain.model.Challenge
-import com.hrhn.presentation.ui.screen.addchallenge.check.CheckEmojiAdapter
-import com.hrhn.presentation.util.observeEvent
-import com.hrhn.presentation.util.showToast
+import com.hrhn.presentation.ui.screen.addchallenge.check.ReviewFragment
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class ReviewActivity : AppCompatActivity() {
@@ -25,19 +23,18 @@ class ReviewActivity : AppCompatActivity() {
         }
     }
 
-    @Inject
-    lateinit var reviewViewModelFactory: ReviewViewModelFactory
-    private val viewModel: ReviewViewModel by viewModels {
-        ReviewViewModel.provideFactory(reviewViewModelFactory, data!!)
-    }
-    private val adapter by lazy { CheckEmojiAdapter { viewModel.checkedChanged(it) } }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initToolbar()
-        initViews()
-        observeData()
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                add(R.id.fcv_review, ReviewFragment.newInstance(data!!))
+            }
+            supportFragmentManager.setFragmentResultListener(ReviewFragment.KEY_REQUEST_EMOJI, this) { _, _ ->
+                finish()
+            }
+        }
     }
 
     private fun initToolbar() {
@@ -46,18 +43,6 @@ class ReviewActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowTitleEnabled(false)
         }
-    }
-
-    private fun initViews() {
-        binding.lifecycleOwner = this
-        binding.vm = viewModel
-        binding.rvEmoji.adapter = adapter
-    }
-
-    private fun observeData() {
-        viewModel.emojis.observe(this) { adapter.submitList(it) }
-        viewModel.finishEvent.observeEvent(this) { finish() }
-        viewModel.message.observeEvent(this) { showToast(it) }
     }
 
     companion object {
