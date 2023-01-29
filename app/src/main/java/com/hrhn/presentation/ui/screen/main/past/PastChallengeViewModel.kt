01a8b22445c0fd3db75fd2e47f1bd626f655ac89
oreocube <oreocube13@gmail.com>
2023-01-29 16:16:15 +0900

    del: 불필요한 코드 제거

--- app/src/main/java/com/hrhn/presentation/ui/screen/main/past/PastChallengeViewModel.kt
+++ app/src/main/java/com/hrhn/presentation/ui/screen/main/past/PastChallengeViewModel.kt
@@ -1,6 +1,10 @@
 package com.hrhn.presentation.ui.screen.main.past
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import androidx.paging.filter
 import com.hrhn.domain.model.Challenge
@@ -8,6 +12,7 @@ import com.hrhn.domain.repository.ChallengeRepository
 import com.hrhn.presentation.util.Event
 import com.hrhn.presentation.util.emit
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import java.time.LocalDate
@@ -15,15 +20,9 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PastChallengeViewModel @Inject constructor(
-    private val repository: ChallengeRepository
+    repository: ChallengeRepository
 ) : ViewModel() {
-    private val _challenges = MutableLiveData<List<Challenge>>(listOf())
-    val challenges: LiveData<List<Challenge>>
-        get() = _challenges
-
-    val isEmpty: LiveData<Boolean> = Transformations.map(challenges) { it.isEmpty() }
-
-    val challengesFlow = repository.challengesFlow
+    val challengesFlow: Flow<PagingData<Challenge>> = repository.challengesFlow
         .map { it.filter { it.date.toLocalDate() != LocalDate.now() } }
         .cachedIn(viewModelScope)
         .catch { e -> e.message?.let { _message.emit(it) } }
@@ -31,14 +30,4 @@ class PastChallengeViewModel @Inject constructor(
     private val _message = MutableLiveData<Event<String>>()
     val message: LiveData<Event<String>> get() = _message
 
-    fun fetchData() {
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.getChallenges()
-                .onSuccess { list ->
-                    _challenges.postValue(list.filter { it.date.toLocalDate() != LocalDate.now() })
-                }.onFailure { t ->
-                    t.message?.let { _message.emit(it) }
-                }
-        }
-    }
 }
