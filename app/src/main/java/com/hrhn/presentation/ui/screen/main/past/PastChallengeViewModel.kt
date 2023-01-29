2f1dcb644226e967e5baff8f233a7454fadb030e
oreocube <oreocube13@gmail.com>
2023-01-29 16:11:39 +0900

    feat: viewModel 수정

--- app/src/main/java/com/hrhn/presentation/ui/screen/main/past/PastChallengeViewModel.kt
+++ app/src/main/java/com/hrhn/presentation/ui/screen/main/past/PastChallengeViewModel.kt
@@ -1,13 +1,15 @@
 package com.hrhn.presentation.ui.screen.main.past
 
 import androidx.lifecycle.*
+import androidx.paging.cachedIn
+import androidx.paging.filter
 import com.hrhn.domain.model.Challenge
 import com.hrhn.domain.repository.ChallengeRepository
 import com.hrhn.presentation.util.Event
 import com.hrhn.presentation.util.emit
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
 import java.time.LocalDate
 import javax.inject.Inject
 
@@ -20,6 +22,11 @@ class PastChallengeViewModel @Inject constructor(
         get() = _challenges
 
     val isEmpty: LiveData<Boolean> = Transformations.map(challenges) { it.isEmpty() }
+
+    val challengesFlow = repository.challengesFlow
+        .map { it.filter { it.date.toLocalDate() != LocalDate.now() } }
+        .cachedIn(viewModelScope)
+        .catch { e -> e.message?.let { _message.emit(it) } }
 
     private val _message = MutableLiveData<Event<String>>()
     val message: LiveData<Event<String>> get() = _message
