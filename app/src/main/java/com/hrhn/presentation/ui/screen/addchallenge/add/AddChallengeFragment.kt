e76ac1243b116831d1b0dfc4b637e3119278b5bc
oreocube <oreocube13@gmail.com>
2023-02-09 16:54:54 +0900

    add: AddChallengeFragment 팩토리 메서드 추가

--- app/src/main/java/com/hrhn/presentation/ui/screen/addchallenge/add/AddChallengeFragment.kt
+++ app/src/main/java/com/hrhn/presentation/ui/screen/addchallenge/add/AddChallengeFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.hrhn.presentation.widget.TodayChallengeWidgetProvider
 import com.hrhn.databinding.FragmentAddChallengeBinding
+import com.hrhn.domain.model.Challenge
+import com.hrhn.presentation.util.customGetSerializable
 import com.hrhn.presentation.util.observeEvent
 import com.hrhn.presentation.util.showToast
 import dagger.hilt.android.AndroidEntryPoint
@@ -56,5 +59,18 @@ class AddChallengeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private const val KEY_CHALLENGE = "challenge"
+        private const val KEY_IS_EDIT = "isEdit"
+
+        fun newInstance(challenge: Challenge = Challenge()) =
+            AddChallengeFragment().apply {
+                arguments = bundleOf(
+                    KEY_CHALLENGE to challenge,
+                    KEY_IS_EDIT to (challenge.id > 0)
+                )
+            }
     }
 }
